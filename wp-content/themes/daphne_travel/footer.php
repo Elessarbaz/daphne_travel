3c25971c8cb4ad160e60a78e0b67cbe5a3ab8b6a
serg <aspirin_1988@mail.ru>
2016-08-05 14:17:58 +0600

    /start

--- wp-content/themes/daphne_travel/footer.php
+++ wp-content/themes/daphne_travel/footer.php
@@ -32,6 +32,16 @@
 <script src="<?php bloginfo('template_directory') ?>/public/js/components/slider.min.js"></script>
 <script src="<?php bloginfo('template_directory') ?>/public/js/components/lightbox.min.js"></script>
 <script src="<?php bloginfo('template_directory') ?>/public/js/app.js"></script>
+<script src="https://bsh.su/client/script/GET/"></script>
+<script>
+	var submitSMG = new BMModule();
+	submitSMG.submitForm(function(success) { $('.blink-mailer input[type=submit]').val('Отправить'); $('.blink-mailer input,.blink-mailer textarea').prop('disabled', true); $('.success-mail-text').html(success); $('.blink-mailer').hide(500);  $('.success-mail-text').show(500);  }, function(error) {});
+</script>
+<script>
+	var el = document.querySelector('input[type="tel"]');
+	console.log();
+	VMasker(el).maskPattern("+9(999) 999-99-99"); // masking the input
+</script>
 <script>
 	$('a[data-country]').click(function () {
 		var country=$(this).data('country');
